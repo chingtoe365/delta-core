736877634b18a252f60066d3c266648e65052ba3
jlong <longjintao59@gmail.com>
2023-11-20 16:21:09 +0000

    feat: Thread-safe dictionary struct and implement update and lock

--- src/domain/task.go
+++ src/domain/task.go
@@ -2,6 +2,8 @@ package domain
 
 import (
 	"context"
+	"delta-core/bootstrap"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -16,14 +18,56 @@ type Task struct {
 	UserID primitive.ObjectID `bson:"userID" json:"-"`
 }
 
+type SafeTaskMap struct {
+	Status   map[string]bool
+	Unlocked chan int
+}
+
+func (stm *SafeTaskMap) Unlock() {
+	stm.Unlocked <- 1
+}
+
+func (stm *SafeTaskMap) Update(taskId string, remove bool) {
+	for {
+		select {
+		case <-stm.Unlocked:
+			// lock it then update the map
+			if remove {
+				delete(stm.Status, taskId)
+			} else {
+				stm.Status[taskId] = true
+			}
+			// fmt.Printf(">> Task map table updated\n")
+			// unlocking through here so it get locked
+			// when map is processing in this select case
+			go stm.Unlock()
+			return
+		default:
+			// still locked
+			// wait for 50 ms and try again
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
 type TaskRepository interface {
 	Create(c context.Context, task *Task) error
+	FetchById(c context.Context, taskId string) (Task, error)
 	FetchByUserID(c context.Context, userID string) ([]Task, error)
 	FetchAll(c context.Context) ([]Task, error)
+	Delete(c context.Context, task *Task) error
 }
 
 type TaskUsecase interface {
 	Create(c context.Context, task *Task) error
 	FetchByUserID(c context.Context, userID string) ([]Task, error)
+	FetchByTaskID(c context.Context, taskId string) (Task, error)
 	FetchAll(c context.Context) ([]Task, error)
+	Delete(c context.Context, task *Task) error
+}
+
+type SignalSubUsecase interface {
+	Subscribe(env *bootstrap.Env, task Task)
+	Unsubscribe(task *Task)
+	InitialiseSingalSubs(env *bootstrap.Env, tasks []Task)
 }
