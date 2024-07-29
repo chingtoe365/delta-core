c8e682744a9047c645ed49ea18272e182228fab8
chingtoe365 <longjintao1990@gmail.com>
2024-07-29 15:24:11 +0100

    fix: Anon access to bypass auth in development mode

--- src/api/middleware/jwt_auth_middleware.go
+++ src/api/middleware/jwt_auth_middleware.go
@@ -4,21 +4,22 @@ import (
 	"net/http"
 	"strings"
 
+	"delta-core/bootstrap"
 	"delta-core/domain"
 	"delta-core/internal/tokenutil"
 
 	"github.com/gin-gonic/gin"
 )
 
-func JwtAuthMiddleware(secret string) gin.HandlerFunc {
+func JwtAuthMiddleware(env *bootstrap.Env) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.Request.Header.Get("Authorization")
 		t := strings.Split(authHeader, " ")
 		if len(t) == 2 {
 			authToken := t[1]
-			authorized, err := tokenutil.IsAuthorized(authToken, secret)
+			authorized, err := tokenutil.IsAuthorized(authToken, env.AccessTokenSecret)
 			if authorized {
-				userID, err := tokenutil.ExtractIDFromToken(authToken, secret)
+				userID, err := tokenutil.ExtractIDFromToken(authToken, env.AccessTokenSecret)
 				if err != nil {
 					c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: err.Error()})
 					c.Abort()
@@ -30,6 +31,11 @@ func JwtAuthMiddleware(secret string) gin.HandlerFunc {
 			}
 			c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: err.Error()})
 			c.Abort()
+			return
+		}
+		if env.AppEnv == "development" {
+			c.Set("x-user-id", env.AnonUserId)
+			c.Next()
 			return
 		}
 		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Not authorized"})
