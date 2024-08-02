84098adfe6aa4a559fa9793c23433b9261a13d78
chingtoe365 <longjintao1990@gmail.com>
2024-08-02 15:40:40 +0100

    refactor: Organize time conversion utils

--- src/api/controller/news_controller.go
+++ src/api/controller/news_controller.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"delta-core/internal"
 	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -31,7 +33,7 @@ type NewsArtical struct {
 // @Description get news via keywords and timespan
 // @Tags News
 // @Param keywords query string true "Keywords"
-// @Param start query string false "Start time"
+// @Param start query string false "Start time - 1 month max from now"
 // @Param end query string false "End time"
 // @Accept json
 // @Produce json
@@ -40,14 +42,30 @@ type NewsArtical struct {
 func (nc *NewsController) Get(c *gin.Context) {
 	var params NewsQueryParams
 	var result []NewsArtical
-	if c.ShouldBindQuery(&params) == nil {
+	err := c.ShouldBindQuery(&params)
+	if err != nil {
 		log.Println(params)
 		log.Println("Error in parsing query parameters")
 		c.JSON(http.StatusBadRequest, result)
 	}
 	// keywords := c.Query("keywords")
 	// stas
+	slog.Info(params.Keywords)
+	slog.Info(params.Start)
+	slog.Info(params.End)
 	query := params.Keywords
+	start, err := internal.ParseInTime(params.Start)
+	if err != nil {
+		slog.Error(err.Error())
+		slog.Error("Failed to parse in start time")
+		c.JSON(http.StatusBadRequest, result)
+	}
+	end, err := internal.ParseInTime(params.End)
+	if err != nil {
+		slog.Error(err.Error())
+		slog.Error("Failed to parse in end time")
+		c.JSON(http.StatusBadRequest, result)
+	}
 
 	httpClient := http.Client{}
 	key := "55669d67fa794458b77130bb3b71aeb4"
@@ -57,7 +75,7 @@ func (nc *NewsController) Get(c *gin.Context) {
 	client, err := api.New(&httpClient, key, url)
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, "News search error")
+		c.JSON(http.StatusInternalServerError, "News search init error")
 	}
 
 	// Create options for Ireland and Business
@@ -70,9 +88,10 @@ func (nc *NewsController) Get(c *gin.Context) {
 	moreOpts := api.Options{
 		Language: "en",
 		Q:        query,
-		SortBy:   "popularity",
-		From:     params.Start,
-		To:       params.End,
+		// Domains:  "bbc.co.uk",
+		SortBy: "popularity",
+		From:   internal.FormatTimeForNewsApi(start),
+		To:     internal.FormatTimeForNewsApi(end),
 	}
 
 	// Get Everything with options from above
@@ -80,6 +99,8 @@ func (nc *NewsController) Get(c *gin.Context) {
 
 	if err != nil {
 		log.Fatalf("Something wrong when fetching news")
+		log.Println(err)
+		// slog.Error(err.Error())
 	}
 
 	for _, artical := range everything.News.Articles {
